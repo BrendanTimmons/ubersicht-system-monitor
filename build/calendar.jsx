80f3a757023303354b0106fe6b8379393eb43b38
Brendan Timmons <brendantimmons@nuraphone.com>
2020-11-25 14:50:53 +1100

    add calendar

--- build/calendar.jsx
+++ build/calendar.jsx
@@ -1,10 +1,32 @@
 "use strict";
+var __makeTemplateObject = (this && this.__makeTemplateObject) || function (cooked, raw) {
+    if (Object.defineProperty) { Object.defineProperty(cooked, "raw", { value: raw }); } else { cooked.raw = raw; }
+    return cooked;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.className = exports.render = exports.refreshFrequency = exports.command = void 0;
-exports.command = '/usr/local/bin/icalBuddy -npn -nc -iep "title,datetime" -ps "| // |" -po "datetime,title" -tf "%H:%M" -li 3 eventsToday';
+// @ts-ignore
+var uebersicht_1 = require("uebersicht");
+exports.command = '/usr/local/bin/icalBuddy -npn -nc -iep "title,datetime" -ps "| // |" -po "datetime,title" -tf "%H:%M" -li 3 -n eventsToday';
 exports.refreshFrequency = 5000; // ms
 exports.render = function (_a) {
     var output = _a.output;
-    return <h1>{output}</h1>;
+    // Clean up the event strings
+    var events = output.replaceAll("• ", "").split("\n");
+    var filteredEvents = events.filter(function (event) { return event.length !== 0; });
+    return (<ol className={list}>
+      {filteredEvents.map(function (event, i) { return (<li className={listItem} key={i}>
+          {event}
+        </li>); })}
+    </ol>);
 };
-exports.className = "\n    left: 20px\n    top: 20px\n    color: #fff\n  ";
+exports.className = {
+    bottom: 10,
+    left: 32,
+    color: "#fff",
+    fontSize: "12px",
+    fontFamily: "Fira Code Retina",
+};
+var list = uebersicht_1.css(templateObject_1 || (templateObject_1 = __makeTemplateObject(["\n  display: flex;\n  padding: 0;\n  margin: 0;\n"], ["\n  display: flex;\n  padding: 0;\n  margin: 0;\n"])));
+var listItem = uebersicht_1.css(templateObject_2 || (templateObject_2 = __makeTemplateObject(["\n  display: block;\n  margin: 0 30px 0 0;\n"], ["\n  display: block;\n  margin: 0 30px 0 0;\n"])));
+var templateObject_1, templateObject_2;
--- src/calendar.tsx
+++ src/calendar.tsx
@@ -1,12 +1,43 @@
+// @ts-ignore
+import { css } from "uebersicht"
 export const command =
-  '/usr/local/bin/icalBuddy -npn -nc -iep "title,datetime" -ps "| // |" -po "datetime,title" -tf "%H:%M" -li 3 eventsToday'
+  '/usr/local/bin/icalBuddy -npn -nc -iep "title,datetime" -ps "| // |" -po "datetime,title" -tf "%H:%M" -li 3 -n eventsToday'
 
 export const refreshFrequency = 5000 // ms
 
-export const render = ({ output }): any => <h1>{output}</h1>
+type Event = string;
 
-export const className = `
-    left: 20px
-    top: 20px
-    color: #fff
-  `
+export const render = ({ output }) => {
+  // Clean up the event strings
+  const events: Event[] = output.replaceAll("• ", "").split("\n")
+  const filteredEvents: Event[] = events.filter((event) => event.length !== 0)
+
+  return (
+    <ol className={list}>
+      {filteredEvents.map((event, i) => (
+        <li className={listItem} key={i}>
+          {event}
+        </li>
+      ))}
+    </ol>
+  )
+}
+
+export const className = {
+  bottom: 10,
+  left: 32,
+  color: "#fff",
+  fontSize: "12px",
+  fontFamily: "Fira Code Retina",
+}
+
+const list = css`
+  display: flex;
+  padding: 0;
+  margin: 0;
+`
+
+const listItem = css`
+  display: block;
+  margin: 0 30px 0 0;
+`
